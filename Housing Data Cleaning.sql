e39d6103b0e966626f43af2591642fc357fc9a46
Rose Njeru <92436079+Rose-njeru@users.noreply.github.com>
2022-11-29 12:32:04 +0300

    Add files via upload
    SQL Syntax

--- Housing Data Cleaning.sql
+++ Housing Data Cleaning.sql
@@ -12,8 +12,12 @@ select PropertyAddress
     FROM datacleaning.`nashville housing data for data cleaning` 
    WHERE PropertyAddress IS NULL;
     -- USING the case statement
-SELECT a1.ParcelID,a1.PropertyAddress,a2.ParcelID,a2.PropertyAddress,
-    CASE WHEN a1.PropertyAddress IS NULL THEN a2.PropertyAddress ELSE a1.PropertyAddress END
+SELECT a1.ParcelID,
+a1.PropertyAddress,
+a2.ParcelID,
+a2.PropertyAddress,
+    CASE 
+    WHEN a1.PropertyAddress IS NULL THEN a2.PropertyAddress ELSE a1.PropertyAddress END
     FROM datacleaning.`nashville housing data for data cleaning` AS a1
 LEFT JOIN
    datacleaning.`nashville housing data for data cleaning` AS a2
@@ -21,7 +25,10 @@ LEFT JOIN
  AND a1.UniqueID<>a2.UniqueID
  WHERE a1.PropertyAddress IS NULL ;
    -- USING coalesce
-   SELECT a1.ParcelID,a1.PropertyAddress,a2.ParcelID,coalesce(a1.PropertyAddress,a2.PropertyAddress)
+   SELECT a1.ParcelID,
+   a1.PropertyAddress,
+   a2.ParcelID,
+   coalesce(a1.PropertyAddress,a2.PropertyAddress)
 FROM datacleaning.`nashville housing data for data cleaning` AS a1
   JOIN
    datacleaning.`nashville housing data for data cleaning` AS a2
@@ -95,7 +102,8 @@ FROM `datacleaning`.`nashville housing data for data cleaning`;
 
 
 SELECT SoldAsVacant,
-CASE WHEN SoldAsVacant ='Y' THEN 'Yes' 
+CASE 
+WHEN SoldAsVacant ='Y' THEN 'Yes' 
 WHEN SoldAsVacant='N' THEN 'NO'
 ELSE SoldAsVacant 
 END
